c45bc8ecd7cc59926fac9b54dd3e7335ffea9898
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-08 10:42:12 +0300

    Add evaluation exports

---
+++ openuwoc_ai/evaluation/__init__.py
@@ -0,0 +1,5 @@
+"""Evaluation metrics for UWOC experiments."""
+
+from openuwoc_ai.evaluation.metrics import bit_error_rate, confidence_interval_normal, symbol_error_rate
+
+__all__ = ["bit_error_rate", "confidence_interval_normal", "symbol_error_rate"]
