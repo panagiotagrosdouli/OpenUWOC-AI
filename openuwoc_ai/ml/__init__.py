138ecfe64aba9b59e39b7c2576d7fa573ef759a6
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-08 10:42:52 +0300

    Add ML module exports

---
+++ openuwoc_ai/ml/__init__.py
@@ -0,0 +1,5 @@
+"""Machine-learning models for AI-assisted UWOC."""
+
+from openuwoc_ai.ml.neural_equalizer import TorchAvailabilityError, NeuralEqualizer
+
+__all__ = ["NeuralEqualizer", "TorchAvailabilityError"]
